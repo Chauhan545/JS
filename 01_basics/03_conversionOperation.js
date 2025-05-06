caf44a54a3f7cf7cd0741ed1a05314b43e69f5c5
Shivani Chauhan <103892929+Chauhan545@users.noreply.github.com>
2025-05-06 17:54:58 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score="99"
+
+console.log(typeof score );
+//bhot baar hume pta he ni hota jaise agar hume frontend request le rahe h
+//
+
+console.log(typeof(score));//as a method
+
+let valueInNumber=Number(score)//score guarented number mai convert ho gya h
+console.log(typeof valueInNumber);
+
+//NaN concept
+//let marks="10lpaZETA"
+//let marks=null
+//let marks=undefined
+let marks=true
+let val=Number(marks)
+console.log(typeof val);
+console.log(val);
+
+//"33"=33
+//"33abc"=NaN
+//true=1; false=0
+
+let isLoggedIn=1
+let boolIsLoggedIn=Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+
+//""=false
+//"shivani"=true
+//1=true, 0=false
+
+let num=33
+let str=String(num)
+console.log(typeof str);
+
+console.log(str);
+
