378d0969c6ab79a2d89e8849506a1b83b00f2461
Shivani Chauhan <103892929+Chauhan545@users.noreply.github.com>
2025-05-06 19:38:43 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// //diff data type
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);//f
+console.log(null==0);//f
+console.log(null>=0);//t
+console.log(undefined==0);//f
+
+// === //strict check
+console.log("2"==2);//t
+console.log("2"===2);//f
